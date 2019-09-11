2105b709696a2f315566acddd18cbe88ff41617b
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-09-11 10:31:34 +0200

    updated Cat class and added a information method

--- src/main/java/se/lexicon/ulf/App.java
+++ src/main/java/se/lexicon/ulf/App.java
@@ -1,6 +1,7 @@
 package se.lexicon.ulf;
 
 import se.lexicon.ulf.models.Animal;
+import se.lexicon.ulf.models.Cat;
 
 public class App 
 {
@@ -9,11 +10,20 @@ public class App
         Animal[] animals = new Animal[3];
         animals[0] = new Animal("Zappy", 3.5);
         animals[1] = new Animal("Scoby", 12.5);
-        animals[2] = new Animal("Findus", 2.3);
+        animals[2] = new Cat("Findus", 2.3, "Skogskatt","longhair");
 
         for (int index = 0; index < animals.length; index++)
         {
-            System.out.println(animals[index]);
+            animals[index].eat();
         }
+
+        for (Animal one : animals)//Foreach
+        {
+            if (one instanceof Cat)//ask animal one if it is a cat
+            {
+                System.out.println( ( (Cat)one ).Information() );//treat animal one as a cat with type cast
+            }
+        }
+
     }
 }
--- src/main/java/se/lexicon/ulf/models/Cat.java
+++ src/main/java/se/lexicon/ulf/models/Cat.java
@@ -1,4 +1,34 @@
 package se.lexicon.ulf.models;
 
-public class Cat {
+public class Cat extends Animal
+{
+    private String breed;
+    private String fur;
+
+    public Cat(String name, double weight, String breed, String fur) {
+        super(name, weight);//Call parent class constructor (must be done on the first line in a constructor)
+
+        this.breed = breed;
+        this.fur = fur;
+    }
+
+    public String Information()
+    {
+        StringBuilder infoString = new StringBuilder();
+
+        infoString.append("-- Info --").append('\n');
+        infoString.append("Name: ").append(getName()).append('\n');
+        infoString.append("Breed: ").append(breed).append('\n');
+        infoString.append("Weight: ").append(getWeight()).append('\n');
+        infoString.append("Fur type: ").append(fur).append('\n');
+        infoString.append("----------").append('\n');
+
+        return infoString.toString();
+    }
+
+    @Override
+    public void eat()
+    {
+        System.out.println( getName() + " the cat eats a fish.");
+    }
 }
